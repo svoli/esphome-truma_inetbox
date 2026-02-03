6cece93173565951ca04c4ad26a5ccd3b626c7c4
svoli <129603254+svoli@users.noreply.github.com>
2026-02-03 09:52:12 +0100

    Update LinBusListener.h
    kompletter austausch des code durch von ChatGPT korrigierten code

--- components/truma_inetbox/LinBusListener.h
+++ components/truma_inetbox/LinBusListener.h
@@ -7,25 +7,30 @@
 #ifdef USE_ESP32
 #include <freertos/FreeRTOS.h>
 #include <freertos/semphr.h>
-#endif  // USE_ESP32
+#endif
+
 #ifdef USE_RP2040
 #include <hardware/uart.h>
 #include <FreeRTOS.h>
 #include <semphr.h>
 #include <queue.h>
-#endif  // USE_RP2040
+#endif
 
-#ifndef  TRUMA_MSG_QUEUE_LENGTH
+#ifndef TRUMA_MSG_QUEUE_LENGTH
 #define TRUMA_MSG_QUEUE_LENGTH 6
 #endif
-#ifndef  TRUMA_LOG_QUEUE_LENGTH
+
+#ifndef TRUMA_LOG_QUEUE_LENGTH
 #define TRUMA_LOG_QUEUE_LENGTH 6
 #endif
 
 namespace esphome {
 namespace truma_inetbox {
 
-enum class LIN_CHECKSUM { LIN_CHECKSUM_VERSION_1, LIN_CHECKSUM_VERSION_2 };
+enum class LIN_CHECKSUM {
+  LIN_CHECKSUM_VERSION_1,
+  LIN_CHECKSUM_VERSION_2
+};
 
 struct QUEUE_LIN_MSG {
   uint8_t current_PID;
@@ -37,8 +42,8 @@ class LinBusListener : public Component, public uart::UARTDevice {
  public:
   float get_setup_priority() const override { return setup_priority::DATA; }
 
-  void dump_config() override;
   void setup() override;
+  void dump_config() override;
   void loop() override;
 
   void set_lin_checksum(LIN_CHECKSUM val) { this->lin_checksum_ = val; }
@@ -50,40 +55,39 @@ class LinBusListener : public Component, public uart::UARTDevice {
   void process_lin_msg_queue(TickType_t xTicksToWait);
   void process_log_queue(TickType_t xTicksToWait);
 
-#ifdef USE_RP2040
-  // Return is the expected wait time till next data check is recommended.
-  u_int32_t onSerialEvent();
-#endif  // USE_RP2040
-
  protected:
+  // ===== UART Byte Handling =====
+  void on_byte_(uint8_t byte);
+
+  // ===== Configuration =====
   LIN_CHECKSUM lin_checksum_ = LIN_CHECKSUM::LIN_CHECKSUM_VERSION_2;
   GPIOPin *cs_pin_ = nullptr;
   GPIOPin *fault_pin_ = nullptr;
   bool observer_mode_ = false;
 
+  // ===== LIN helpers =====
   void write_lin_answer_(const uint8_t *data, uint8_t len);
   bool check_for_lin_fault_();
   virtual bool answer_lin_order_(const uint8_t pid) = 0;
-  virtual void lin_message_recieved_(const uint8_t pid, const uint8_t *message, uint8_t length) = 0;
+  virtual void lin_message_recieved_(const uint8_t pid,
+                                     const uint8_t *message,
+                                     uint8_t length) = 0;
 
  private:
-  // Microseconds per UART Baud
-  u_int32_t time_per_baud_;
-  // 9.. 15
+  // ===== Timing =====
+  uint32_t time_per_baud_;
   const uint8_t lin_break_length = 13;
-  // Microseconds per LIN Break
-  u_int32_t time_per_lin_break_;
-  const uint8_t frame_length_ = (8 /* bits */ + 1 /* Start bit */ + 2 /* Stop bits */);
-  // Microseconds per UART Byte (UART Frame)
-  u_int32_t time_per_pid_;
-  // Microseconds per UART Byte (UART Frame)
-  u_int32_t time_per_first_byte_;
-  // Microseconds per UART Byte (UART Frame)
-  u_int32_t time_per_byte_;
+  uint32_t time_per_lin_break_;
+  const uint8_t frame_length_ = (8 + 1 + 2);
+  uint32_t time_per_pid_;
+  uint32_t time_per_first_byte_;
+  uint32_t time_per_byte_;
 
+  // ===== Fault handling =====
   uint8_t fault_on_lin_bus_reported_ = 0;
   bool can_write_lin_answer_ = false;
 
+  // ===== LIN state machine =====
   enum read_state {
     READ_STATE_BREAK,
     READ_STATE_SYNC,
@@ -91,59 +95,47 @@ class LinBusListener : public Component, public uart::UARTDevice {
     READ_STATE_DATA,
     READ_STATE_ACT,
   };
+
   read_state current_state_ = READ_STATE_BREAK;
   uint8_t current_PID_with_parity_ = 0x00;
   uint8_t current_PID_ = 0x00;
   bool current_PID_order_answered_ = false;
-  bool current_data_valid = true;
+  bool current_data_valid_ = true;
   uint8_t current_data_count_ = 0;
-  // up to 8 byte data frame + CRC
   uint8_t current_data_[9] = {};
-  // // Time when the last LIN data was available.
   uint32_t last_data_recieved_ = 0;
 
   void current_state_reset_() {
-    this->current_state_ = READ_STATE_BREAK;
-    this->current_PID_with_parity_ = 0x00;
-    this->current_PID_ = 0x00;
-    this->current_PID_order_answered_ = false;
-    this->current_data_valid = true;
-    this->current_data_count_ = 0;
-    memset(this->current_data_, 0, sizeof(this->current_data_));
-  };
-  void onReceive_();
+    current_state_ = READ_STATE_BREAK;
+    current_PID_with_parity_ = 0x00;
+    current_PID_ = 0x00;
+    current_PID_order_answered_ = false;
+    current_data_valid_ = true;
+    current_data_count_ = 0;
+    memset(current_data_, 0, sizeof(current_data_));
+  }
+
   void read_lin_frame_();
   void clear_uart_buffer_();
-  void setup_framework();
 
+  // ===== Queues =====
   uint8_t lin_msg_static_queue_storage[TRUMA_MSG_QUEUE_LENGTH * sizeof(QUEUE_LIN_MSG)];
   StaticQueue_t lin_msg_static_queue_;
   QueueHandle_t lin_msg_queue_ =
-      xQueueCreateStatic(/* uxQueueLength */ TRUMA_MSG_QUEUE_LENGTH,
-                         /* uxItemSize */ sizeof(QUEUE_LIN_MSG),
-                         /* pucQueueStorageBuffer */ lin_msg_static_queue_storage, &lin_msg_static_queue_);
+      xQueueCreateStatic(TRUMA_MSG_QUEUE_LENGTH,
+                         sizeof(QUEUE_LIN_MSG),
+                         lin_msg_static_queue_storage,
+                         &lin_msg_static_queue_);
 
 #if ESPHOME_LOG_LEVEL > ESPHOME_LOG_LEVEL_NONE
   uint8_t log_static_queue_storage[TRUMA_LOG_QUEUE_LENGTH * sizeof(QUEUE_LOG_MSG)];
   StaticQueue_t log_static_queue_;
   QueueHandle_t log_queue_ =
-      xQueueCreateStatic(/* uxQueueLength */ TRUMA_LOG_QUEUE_LENGTH,
-                         /* uxItemSize */ sizeof(QUEUE_LOG_MSG),
-                         /* pucQueueStorageBuffer */ log_static_queue_storage, &log_static_queue_);
+      xQueueCreateStatic(TRUMA_LOG_QUEUE_LENGTH,
+                         sizeof(QUEUE_LOG_MSG),
+                         log_static_queue_storage,
+                         &log_static_queue_);
 #endif
-
-#ifdef USE_ESP32
-  TaskHandle_t eventTaskHandle_;
-  static void eventTask_(void *args);
-#endif  // USE_ESP32
-#ifdef USE_ESP32_FRAMEWORK_ESP_IDF
-  TaskHandle_t uartEventTaskHandle_;
-  static void uartEventTask_(void *args);
-#endif  // USE_ESP32_FRAMEWORK_ESP_IDF
-#ifdef USE_RP2040
-  uint8_t uart_number_ = 0;
-  uart_inst_t *uart_ = nullptr;
-#endif  // USE_RP2040
 };
 
 }  // namespace truma_inetbox
