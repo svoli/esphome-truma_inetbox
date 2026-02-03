9b3efb6b877044794a68358890bbff3d7f7f4d91
svoli <129603254+svoli@users.noreply.github.com>
2026-02-03 12:42:26 +0100

    Update TrumaRoomClimate.cpp

--- components/truma_inetbox/climate/TrumaRoomClimate.cpp
+++ components/truma_inetbox/climate/TrumaRoomClimate.cpp
@@ -131,7 +131,7 @@ climate::ClimateTraits TrumaRoomClimate::traits() {
   climate::ClimateTraits traits;
 
   // Aktuelle Temperatur unterstützen
-  traits.add_feature_flags(climate::ClimateFeature::SUPPORTS_CURRENT_TEMPERATURE);
+  traits.add_feature_flags(climate::ClimateFeature::CLIMATE_SUPPORTS_CURRENT_TEMPERATURE);
 
   // Unterstützte Modi
   climate::ClimateModeMask mode_mask;
